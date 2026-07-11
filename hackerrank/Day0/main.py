2c7930150d256f9b789e5002ec8af9ee465c8ddb
YSofty <j.cedenoparedes18@gmail.com>
2026-07-11 14:35:53 +0000

    Se modifico la linea 8 en lenguaje python

--- hackerrank/Day0/main.py
+++ hackerrank/Day0/main.py
@@ -1,7 +1,8 @@
-inputString = input('Ponga un mensaje:') # get a line of input from stdin and save it to our variable
+# get a line of input from stdin and save it to our variable
+usuario = input('Ponga un mensaje: ')
 
 # Your first line of output goes here
 print('Hola, Mundo.')
 
 # Write the second line of output
-print (inputString)
+print (f'Mensaje dirigido del la persona: {usuario}')
