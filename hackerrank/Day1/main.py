8f45bcfbb3b6b381ebaf0613fd919f5d56364431
YSofty <j.cedenoparedes18@gmail.com>
2026-07-13 04:39:06 +0000

    Se agrego el reto del day 1 data type en python

---
+++ hackerrank/Day1/main.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+entero = 0
+decimal = 0.0
+texto = ''
+# Read and save an integer, double, and String to your variables.
+entero = int(input('Ingrese un número entero: '))
+decimal = float(input('Ingrese un número decimal: '))
+texto = input('Ingrese un texto: ')
+
+# Read and save a
+# Print the sum of both integer variables on a new line.
+print(f'La suma de los numeros enteros: {i + entero}')
+
+# Print the sum of the double variables on a new line.
+print(f'La suma de los numeros decimales: {d + decimal}')
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(f'La concatenacion entre los textos es: {s}{texto}')
